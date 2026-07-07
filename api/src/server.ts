dedda67a43bad35c221e141399b854be64c3147a
vxtinzz <vitin.ti679@gmail.com>
2026-07-07 09:49:17 -0300

    fix(server):fix const PORT

--- api/src/server.ts
+++ api/src/server.ts
@@ -4,7 +4,7 @@ import authRoutes from "./routes/auth.routes"
 import adminRoutes from "./routes/admin.routes"
 
 const app = express()
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(express.json({limit : "10kb"}))
 app.use("/user", userRoutes)
