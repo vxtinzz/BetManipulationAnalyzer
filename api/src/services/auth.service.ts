656e13fd5de5f76f2a0b5bd3df70cfb6f18392ae
vxtinzz <vitin.ti679@gmail.com>
2026-08-12 17:23:48 -0300

    feat: create refreshToken function

--- api/src/services/auth.service.ts
+++ api/src/services/auth.service.ts
@@ -4,6 +4,7 @@ import "dotenv/config"
 import * as userRepository from "../repositories/user.repository"
 import * as refreshRepository from "../repositories/refresh.repository"
 import { validateUserCreate, validateUserLogin } from "../utils/validators"
+import { hash } from "node:crypto"
 
 export async function registerUser(data: any) {
   validateUserCreate(data)
@@ -52,7 +53,7 @@ export async function loginUser(data: any) {
      const expiresAt = new Date();
      expiresAt.setDate(expiresAt.getDate() + 7);
 
-     await refreshRepository.refreshTokenRepository.create({
+     await refreshRepository.create({
       tokenHash: refreshTokenHash,
       userId: foundedUser.id,
       expiresAt: expiresAt
@@ -69,8 +70,69 @@ export async function loginUser(data: any) {
     };
 
  } catch (error) {
-   throw error;
+    throw error;
  }
+}
 
-//refresh function
+export async function refresh(refreshToken: string) {
+  try{
+      const payload = jwt.verify(refreshToken, process.env.SECRET_KEY!, { algorithms:['HS256']}) as JwtPayload;
+      if(payload.type !== 'refresh'){
+         throw new Error("Unauthorized")
+      }
+      
+      const foundedUser = await userRepository.findById(payload.id);
+
+      if(!foundedUser){
+        throw new Error("Invalid Refresh Token")
+      }
+
+      const foundedHash = await refreshRepository.findByUserId(foundedUser.id);
+      let validToken = null;
+
+      for (const hashTest of foundedHash) {
+        const isValid = await bcrypt.compare(refreshToken, hashTest.tokenHash);
+
+        if(isValid){
+          validToken = hashTest;
+          break;
+        }
+      }
+
+      if(!validToken){
+        throw new Error("Invalid Refresh Token")
+      }     
+
+      const newAcessToken = jwt.sign({ id: foundedUser.id.toString(), username: foundedUser.username, role: foundedUser.role, type: "acess"}, process.env.SECRET_KEY!, {
+       expiresIn: '1h',
+     });
+
+     const newRefreshToken = jwt.sign({ id: foundedUser.id.toString(), type: "refresh" }, process.env.SECRET_KEY!, {
+       expiresIn: '7d',
+     });
+
+     const newRefreshTokenHash = await bcrypt.hash(newRefreshToken, 10);
+     const expiresAt = new Date();
+     expiresAt.setDate(expiresAt.getDate() + 7);
+
+     await refreshRepository.create({
+      tokenHash: newRefreshTokenHash,
+      userId: foundedUser.id,
+      expiresAt: expiresAt
+     })
+
+     await refreshRepository.revokeTokenById(validToken.id)
+
+     return { 
+      user: { 
+        id: foundedUser.id, 
+        username:foundedUser.username,
+        role:foundedUser.role
+      },
+      acessToken: newAcessToken,
+      refreshToken: newRefreshToken
+    };
+  } catch (error){
+    throw error;
+  }
 }
