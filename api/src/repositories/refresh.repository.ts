e5af0b3bae6bb57ace408aa04eedcf8479d8abdb
vxtinzz <vitin.ti679@gmail.com>
2026-08-12 17:22:35 -0300

    feat: create revokeToken function and findTokenById

--- api/src/repositories/refresh.repository.ts
+++ api/src/repositories/refresh.repository.ts
@@ -1,11 +1,32 @@
 import prisma from "../config/prisma";
 
-export const refreshTokenRepository = {
-  async create(data: {
+export function create(data: {
     tokenHash: string;
     userId: string;
     expiresAt: Date;
-  }) {
+  }){
     return prisma.refreshToken.create({ data });
   }
-};
+
+export function findByUserId(userId: string) {
+  return prisma.refreshToken.findMany({
+    where: {
+      userId,
+      revokedAt: null,
+      expiresAt: {
+        gt: new Date(),
+      },
+    },
+  });
+}
+
+export function revokeTokenById(tokenId: string) {
+  return prisma.refreshToken.update({
+    where: {
+      id: tokenId,
+    },
+    data: {
+      revokedAt: new Date(),
+    },
+  });
+}
