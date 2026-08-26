c38b3be71f6ba366a91bf8ed62f67da438500635
vxtinzz <vitin.ti679@gmail.com>
2026-08-26 10:17:51 -0300

    fix: update get routes

--- api/src/routes/admin.routes.ts
+++ api/src/routes/admin.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { createUser, adminGetUser, adminGetUsers, getUser ,adminUpdateUser, updateUser, deleteUser, adminDeleteUser } from "../controllers/user.controller"
+import { createUser, adminGetUserById, adminGetUsers, getAdminMe ,adminUpdateUser, updateUser, deleteUser, adminDeleteUser } from "../controllers/user.controller"
 import { auth, authorize } from "../middleware/auth.middleware"
 
 const router = Router()
@@ -7,10 +7,10 @@ const router = Router()
 //admin
 router.post("/user", auth, authorize("admin"), createUser)
 router.get("/users", auth, authorize("admin"), adminGetUsers)
-router.get("/user/:id", auth, authorize("admin"), adminGetUser)
+router.get("/user/:id", auth, authorize("admin"), adminGetUserById)
 router.patch("/user/:id", auth, authorize("admin"), adminUpdateUser)
 router.delete("/user/:id", auth, authorize("admin"), adminDeleteUser)
-router.get("/me", auth, authorize("admin"), getUser)
+router.get("/me", auth, authorize("admin"), getAdminMe)
 router.patch("/me", auth, authorize("admin"), updateUser)
 router.delete("/me", auth, authorize("admin"), deleteUser)
 
